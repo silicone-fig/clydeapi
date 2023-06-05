07ea117a3947cd51b2ea86d5fd7310e00ca551eb
silicone-fig <94190630+silicone-fig@users.noreply.github.com>
2023-06-05 15:51:44 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,3 @@ app = Flask(__name__)
 @app.route("/api/v1/chat/completions")
 def test():
     return "make the chatgpt stuff!"
-
-@app.route("/api/v1/processing")
-def test():
-    return "working api!"
