55ca8882afe2b529be748fec93f9b45cf0b2d64d
silicone-fig <94190630+silicone-fig@users.noreply.github.com>
2023-06-05 16:45:25 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Page = () => {
   return (
-    <div style={{ backgroundColor: 'darkblue', color: 'white', padding: '20px' }}>
+    <div style={{ backgroundColor: '#38465e', color: 'white', padding: '20px' }}>
       <h1>You're looking for the completions API</h1>
       <p>/api/v1/chat/completions</p>
     </div>
